c66851c8c01f72e63233a88ccda307fba7d86f62
Leonardo Garzon <l.garzonr@unianes.edu.co>
2022-12-27 20:48:12 -0500

    formated

--- cognito.tf
+++ cognito.tf
@@ -1,48 +1,48 @@
 resource "aws_cognito_user_pool" "dermoapp_patients" {
-  name = "dermoapp_patients_test"
-  username_attributes      = ["email"]
+  name                = "dermoapp_patients_test"
+  username_attributes = ["email"]
   password_policy {
-    minimum_length = 8
-    require_numbers = true
+    minimum_length    = 8
+    require_numbers   = true
     require_uppercase = true
   }
 
   account_recovery_setting {
     recovery_mechanism {
-      name = "verified_email"
+      name     = "verified_email"
       priority = 1
     }
   }
 
   schema {
-    attribute_data_type      = "String"
-    name                     = "email"
-    required = true
+    attribute_data_type = "String"
+    name                = "email"
+    required            = true
     string_attribute_constraints {
       min_length = 1
       max_length = 2048
     }
   }
   schema {
-    attribute_data_type      = "String"
-    name                     = "name"
-    required = true
+    attribute_data_type = "String"
+    name                = "name"
+    required            = true
     string_attribute_constraints {
       min_length = 1
       max_length = 2048
     }
   }
   schema {
-    attribute_data_type      = "String"
-    name                     = "birthdate"
-    required = true
+    attribute_data_type = "String"
+    name                = "birthdate"
+    required            = true
     string_attribute_constraints {
       min_length = 1
       max_length = 2048
     }
   }
   schema {
-    name = "city"
+    name                = "city"
     attribute_data_type = "String"
     string_attribute_constraints {
       min_length = 1
@@ -51,14 +51,14 @@ resource "aws_cognito_user_pool" "dermoapp_patients" {
   }
 
   verification_message_template {
-    default_email_option = "CONFIRM_WITH_LINK"
+    default_email_option  = "CONFIRM_WITH_LINK"
     email_subject_by_link = "Bienvenido a dermoApp, por favor valide su correo"
     email_message_by_link = "Bienvenido a dermoApp, Gracias por ser un usuario de dermoApp, para poder continuar es necesario validar su correo electrónico antes de usar la aplicación. {##validar correo##} "
   }
 }
 
 resource "aws_cognito_user_pool_client" "client_patients_tf" {
-  name = "client_patients_tf"
+  name         = "client_patients_tf"
   user_pool_id = aws_cognito_user_pool.dermoapp_patients.id
 }
 
